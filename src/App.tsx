afb0468be6428ff6a3eafeb8283a376427436c62
lhohw <lho.hw.an1@gmail.com>
2022-10-05 01:10:34 +0900

    selectStart preventDefault

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,14 @@ import Corporate from "features/corporate/Corporate";
 
 import { useAppSelector } from "app/hooks";
 
+document
+  .querySelector("body")!
+  .addEventListener("selectstart", (e) => e.preventDefault());
+
 const App = () => {
   const { list, selected } = useAppSelector((state) => state.reader);
   const { year, corporate } = selected;
+
   return (
     <div style={{ paddingBottom: "1rem" }}>
       <Reader />
