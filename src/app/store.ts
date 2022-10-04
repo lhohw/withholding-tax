eaef119364760c607bcb2008b976fa798d397586
lhohw <lho.hw.an1@gmail.com>
2022-10-05 01:08:52 +0900

    store - rootReducer 분리를 통해 RootState type 순환참조 문제 해결

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,21 +1,28 @@
-import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import {
+  configureStore,
+  ThunkAction,
+  Action,
+  combineReducers,
+} from "@reduxjs/toolkit";
 
 import readerReducer from "features/reader/readerSlice";
 import personReducer from "features/person/personSlice";
 import corporateReducer from "features/corporate/corporateSlice";
 import infoReducer from "features/info/infoSlice";
 
+const rootReducer = combineReducers({
+  reader: readerReducer,
+  person: personReducer,
+  corporate: corporateReducer,
+  info: infoReducer,
+});
+
 export const store = configureStore({
-  reducer: {
-    reader: readerReducer,
-    person: personReducer,
-    corporate: corporateReducer,
-    info: infoReducer,
-  },
+  reducer: rootReducer,
 });
 
 export type AppDispatch = typeof store.dispatch;
-export type RootState = ReturnType<typeof store.getState>;
+export type RootState = ReturnType<typeof rootReducer>;
 export type AppThunk<ReturnType = void> = ThunkAction<
   ReturnType,
   RootState,
