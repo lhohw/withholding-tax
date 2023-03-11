6c3f0e24518efa41c50538b911fd79002dc4db4e
lhohw <lho.hw.an1@gmail.com>
2023-03-12 03:35:17 +0900

    refactor: seperate of interests

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import ErrorBoundary from "components/ErrorBoundary";
 import Header from "components/Header";
+import Table from "components/Table";
 
 const App = () => {
   return (
     <ErrorBoundary>
       <Header />
+      <Table />
     </ErrorBoundary>
   );
 };
