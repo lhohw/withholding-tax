c780c5cc243540e430373b2db45d30166493e678
lhohw <lho.hw.an1@gmail.com>
2023-03-19 23:32:27 +0900

    feat: make common type for yearly data and method for table data

--- src/models/Employee.ts
+++ src/models/Employee.ts
@@ -1,8 +1,12 @@
-import type { CorporateT } from "./Corporate";
+import Corporate from "./Corporate";
+import { parseMoney } from "lib/utils";
 
 export type YYYYMMDD = `${string}.${string}.${string}`;
 export type WorkingDate = Record<"start" | "resign", YYYYMMDD>;
 export type Salary = Record<"youth" | "manhood", number>;
+export type YearlyData<T> = {
+  [year: string]: T;
+};
 
 export type MonthlyStatementOfPaymentOfWageAndSalary = {
   salary: {
@@ -32,40 +36,18 @@ export type MonthlyStatementOfPaymentOfWageAndSalary = {
   };
 };
 
-export type EmployeeT = {
-  id: string;
-  name: string;
-  birth: YYYYMMDD;
-  corporate: Pick<CorporateT, "name" | "RN" | "address">;
-  earnedIncomeWithholdingDepartment: {
-    [year: string]: MonthlyStatementOfPaymentOfWageAndSalary[];
-  };
-  date: {
-    [year: string]: WorkingDate;
-  };
-  salary: {
-    [year: string]: Salary;
-  };
-};
-
-class Employee implements EmployeeT {
-  public salary: {
-    [year: string]: Salary;
-  };
+class Employee {
   constructor(
     public id: string,
     public birth: YYYYMMDD,
     public name: string,
-    public corporate: Pick<CorporateT, "name" | "RN" | "address">,
-    public date: {
-      [year: string]: WorkingDate;
-    },
-    public earnedIncomeWithholdingDepartment: {
-      [year: string]: MonthlyStatementOfPaymentOfWageAndSalary[];
-    },
-    public year: string
-  ) {
-    this.salary = {
+    public corporate: Pick<Corporate, "name" | "RN" | "address">,
+    public date: YearlyData<WorkingDate>,
+    public earnedIncomeWithholdingDepartment: YearlyData<
+      MonthlyStatementOfPaymentOfWageAndSalary[]
+    >,
+    public year: string,
+    public salary: YearlyData<Salary> = {
       [year]: {
         youth: earnedIncomeWithholdingDepartment[year].reduce(
           (acc, x) => acc + x.salary.youth,
@@ -76,7 +58,35 @@ class Employee implements EmployeeT {
           0
         ),
       },
-    };
+    }
+  ) {}
+  getTableData(year: string) {
+    const { name, date, birth, earnedIncomeWithholdingDepartment, salary } =
+      this;
+    if (!this.earnedIncomeWithholdingDepartment[year]) {
+      return [
+        name,
+        "",
+        "",
+        birth.slice(2),
+        "0",
+        "0",
+        ...new Array(12).fill("-"),
+      ];
+    }
+    const { start, resign } = date[year];
+    const { youth, manhood } = salary[year];
+    return [
+      name,
+      birth.slice(2),
+      start.slice(2),
+      resign.slice(2),
+      parseMoney(youth),
+      parseMoney(manhood),
+      ...earnedIncomeWithholdingDepartment[year].map((e) =>
+        e.salary.youth ? "청년" : e.salary.manhood ? "장년" : "-"
+      ),
+    ];
   }
 }
 
