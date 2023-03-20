81e04ba812319843e7833db60ad81f3f1082e6e8
lhohw <lho.hw.an1@gmail.com>
2023-03-20 20:30:50 +0900

    design: move to absolute position of left top of statistics

--- src/components/MonthCounter.tsx
+++ src/components/MonthCounter.tsx
@@ -3,7 +3,6 @@ import { css } from "@emotion/react";
 
 import useTable from "hooks/useTable";
 import { exceptNumberRegex } from "constants/regex";
-import { whiteNeumorphism } from "styles/neumorphism";
 import { lightOrangeGradient } from "styles/gradient";
 
 export type MonthCounterProps = {
@@ -21,76 +20,93 @@ const MonthCounter = ({ RN, year }: MonthCounterProps) => {
     [setMonth]
   );
   return (
-    <div
-      css={css`
-        ${whiteNeumorphism("8px")};
-        display: flex;
-        flex-direction: column;
-        height: 120px;
-        width: 100px;
-        justify-content: center;
-        margin-left: 3rem;
-        border: 1px solid var(--placeholder);
-      `}
-    >
-      <div
-        css={css`
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          height: 40px;
-          border-bottom: 1px dashed var(--navy);
-          border-radius: 8px 8px 0 0;
-          background: ${lightOrangeGradient};
-          font-size: 0.9rem;
-          font-weight: bold;
-        `}
-      >
-        Month
-      </div>
-      <div
-        css={css`
-          display: flex;
-          flex: 1;
-          flex-direction: row;
-          align-items: center;
-          justify-content: center;
-          background-color: rgba(254, 254, 254, 0.376);
-          font-size: 1.5rem;
-          padding: 0 0.5rem;
-          border-radius: 0 0 8px 8px;
-        `}
-      >
-        <span
-          css={css`
-            font-weight: bold;
-          `}
-        >
-          /
-        </span>
-        <input
-          css={css`
-            display: flex;
-            background: transparent;
-            border: none;
-            border-bottom: 2px solid var(--black);
-            text-align: center;
-            height: 40px;
-            width: 30px;
-            margin-left: 1rem;
-            transition: border-color, font-weight 0.25s ease-in-out;
-            &:focus {
-              outline: none;
-              border-color: var(--light-orange);
-              font-weight: bold;
-            }
-          `}
-          value={month}
-          onChange={onMonthChange}
-        />
-      </div>
-    </div>
+    <MonthCounterWrapper>
+      <MonthTitle />
+      <Splitter />
+      <MonthInput month={month} onMonthChange={onMonthChange} />
+    </MonthCounterWrapper>
   );
 };
 
+const MonthCounterWrapper = ({ children }: { children: React.ReactNode }) => (
+  <div
+    css={css`
+      ${lightOrangeGradient};
+      display: flex;
+      flex-direction: row;
+      height: 35px;
+      width: 110px;
+      margin: 2.5px;
+      justify-content: center;
+      border: 0.5px solid var(--navy);
+      border-radius: 4px;
+      position: absolute;
+      left: 0;
+      top: 0;
+    `}
+  >
+    {children}
+  </div>
+);
+const MonthTitle = () => (
+  <div
+    css={css`
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      border-radius: 8px 8px 0 0;
+      font-size: 0.7rem;
+      font-weight: bold;
+      position: absolute;
+      left: 0.2rem;
+      top: 0.2rem;
+    `}
+  >
+    Month
+  </div>
+);
+const Splitter = () => (
+  <div
+    css={css`
+      position: absolute;
+      left: 47%;
+      top: 50%;
+      width: 27px;
+      height: 1px;
+      border: 1px solid var(--black);
+      transform: translateX(-50%) rotate(135deg);
+    `}
+  />
+);
+
+const MonthInput = ({
+  month,
+  onMonthChange,
+}: {
+  month: any;
+  onMonthChange: any;
+}) => (
+  <input
+    css={css`
+      display: flex;
+      background-color: var(--white);
+      border: none;
+      border-bottom: 2px solid var(--black);
+      text-align: center;
+      width: 40px;
+      height: 27.5px;
+      position: absolute;
+      top: 4px;
+      right: 2px;
+      transition: border-color, font-weight 0.25s ease-in-out;
+      font-weight: bold;
+      &:focus {
+        outline: none;
+        border-color: var(--light-orange);
+      }
+    `}
+    value={month}
+    onChange={onMonthChange}
+  />
+);
 export default MonthCounter;
