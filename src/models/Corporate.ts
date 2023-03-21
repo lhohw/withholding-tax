14e2dd8a720b1c47037162c2b197bc038e862985
lhohw <lho.hw.an1@gmail.com>
2023-03-21 10:32:33 +0900

    refactor: move getTableData logic from atom to model

--- src/models/Corporate.ts
+++ src/models/Corporate.ts
@@ -1,5 +1,11 @@
 import Employee from "./Employee";
 
+export type CorporateProps = {
+  RN: string;
+  name: string;
+  address: string;
+  employees: { [id: string]: Employee };
+};
 class Corporate {
   public RN: string;
   public name: string;
@@ -7,12 +13,43 @@ class Corporate {
   public employees: {
     [id: string]: Employee;
   };
-  constructor(props: Corporate) {
-    const { RN, name, address, employees } = props;
+  constructor({ RN, name, address, employees }: CorporateProps) {
     this.RN = RN;
     this.name = name;
     this.address = address;
     this.employees = employees;
+  }
+  getTableResult(year: string) {
+    const { employees } = this;
+    const ret = {
+      total: new Array(14).fill(0),
+      youth: new Array(14).fill(0),
+      manhood: new Array(14).fill(0),
+    };
+
+    const PADDING = 2;
+
+    for (const { salary, earnedIncomeWithholdingDepartment } of Object.values(
+      employees
+    )) {
+      if (!(year in salary)) continue;
+      const { youth, manhood } = salary[year];
+      const monthlyData = earnedIncomeWithholdingDepartment[year];
+      ret.total[1] += youth + manhood;
+      for (let month = 0; month < 12; month++) {
+        const {
+          salary: { youth, manhood },
+        } = monthlyData[month];
+        const youthVal = Number(youth !== 0);
+        const manhoodVal = Number(manhood !== 0);
+        ret.total[PADDING + month] += youthVal + manhoodVal;
+        ret.youth[PADDING + month] += youthVal;
+        ret.youth[0] += youth;
+        ret.manhood[PADDING + month] += manhoodVal;
+        ret.manhood[1] += manhood;
+      }
+    }
+    return ret;
   }
 }
 
--- src/recoil/table/atom.ts
+++ src/recoil/table/atom.ts
@@ -27,36 +27,8 @@ export const resultState = atomFamily<ResultState, ResultProps>({
     get:
       ({ RN, year }) =>
       ({ get }) => {
-        const { employees } = get(corporatesState)[RN];
-        const ret = {
-          total: new Array(14).fill(0),
-          youth: new Array(14).fill(0),
-          manhood: new Array(14).fill(0),
-        };
-
-        const PADDING = 2;
-
-        for (const {
-          salary,
-          earnedIncomeWithholdingDepartment,
-        } of Object.values(employees)) {
-          if (!(year in salary)) continue;
-          const { youth, manhood } = salary[year];
-          const monthlyData = earnedIncomeWithholdingDepartment[year];
-          ret.total[1] += youth + manhood;
-          for (let month = 0; month < 12; month++) {
-            const {
-              salary: { youth, manhood },
-            } = monthlyData[month];
-            const youthVal = Number(youth !== 0);
-            const manhoodVal = Number(manhood !== 0);
-            ret.total[PADDING + month] += youthVal + manhoodVal;
-            ret.youth[PADDING + month] += youthVal;
-            ret.youth[0] += youth;
-            ret.manhood[PADDING + month] += manhoodVal;
-            ret.manhood[1] += manhood;
-          }
-        }
+        const corporate = get(corporatesState)[RN];
+        const ret = corporate.getTableResult(year);
         return ret;
       },
   }),
