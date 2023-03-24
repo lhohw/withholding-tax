5676e44106a0d95e7cce76981289d948c41e7c73
lhohw <lho.hw.an1@gmail.com>
2023-03-24 17:44:15 +0900

    fix: Move default checked setting logic to employee recoil

--- src/hooks/useEmployee.ts
+++ src/hooks/useEmployee.ts
@@ -5,50 +5,25 @@ import Employee from "models/Employee";
 import { employeeCheckedState } from "recoil/employee";
 import { workingDaysState } from "recoil/employee/atom";
 
-import { getDays, isRetired } from "lib/utils";
-import { useEffect } from "react";
-
 export type UseEmployeeProps = {
   employee: InstanceType<typeof Employee>;
   year: string;
 };
 const useEmployee = ({ employee, year }: UseEmployeeProps) => {
-  const getDefaultEmployeeChecked = ({ employee, year }: UseEmployeeProps) => {
-    const { earnedIncomeWithholdingDepartment, date } = employee;
-    if (!(year in earnedIncomeWithholdingDepartment))
-      return new Array(12).fill(false);
-
-    const resign = date[year]?.resign;
-    return earnedIncomeWithholdingDepartment[year].map(
-      ({ totalSalary: { total } }, idx) => {
-        if (!date[year]?.start) return false;
-
-        const startDate = Date.parse(date[year].start);
-        const monthsLastDay = Date.parse(
-          `${year}.${(idx + 1).toString().padStart(2, "0")}.${getDays(
-            +year,
-            idx
-          )}`
-        );
-        if (startDate > monthsLastDay) return false;
-        if ((!resign || Date.parse(resign) > monthsLastDay) && total === 0)
-          return true;
-        if (resign && isRetired(resign, year, idx + 1) && total) return true;
-        return false;
-      }
-    );
-  };
-
   const [checked, setChecked] = useRecoilState(
-    employeeCheckedState({ id: employee.id, year })
+    employeeCheckedState({
+      id: employee.id,
+      year,
+      date: employee.date,
+      mappedEarnedIncomeWithholdingDepartment:
+        employee.earnedIncomeWithholdingDepartment[year]?.map(
+          (e) => e.totalSalary.total
+        ),
+    })
   );
   const workingDays = useRecoilValue(
     workingDaysState({ date: employee.date, year })
   );
-  useEffect(() => {
-    setChecked(getDefaultEmployeeChecked({ employee, year }));
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
   return {
     checked,
     setChecked,
--- src/recoil/employee/atom.ts
+++ src/recoil/employee/atom.ts
@@ -2,12 +2,54 @@ import { atomFamily } from "recoil";
 
 import Employee from "models/Employee";
 
-import { getWorkingDays } from "lib/utils";
+import { getDays, getWorkingDays, isRetired } from "lib/utils";
 
-export const employeeCheckedState = atomFamily({
+export type EmployeeCheckedStateProps = {
+  id: string;
+  year: string;
+  date: Employee["date"];
+  mappedEarnedIncomeWithholdingDepartment?: number[];
+};
+export const employeeCheckedState = atomFamily<
+  boolean[],
+  EmployeeCheckedStateProps
+>({
   key: "EmployeeCheckedState",
-  default: new Array(12).fill(false),
+  default: ({ year, date, mappedEarnedIncomeWithholdingDepartment }) => {
+    if (!mappedEarnedIncomeWithholdingDepartment)
+      return new Array(12).fill(false);
+    return getDefaultEmployeeChecked({
+      year,
+      date,
+      mappedEarnedIncomeWithholdingDepartment,
+    });
+  },
 });
+
+export type GetDefaultEmployeeCheckedProps = Pick<
+  EmployeeCheckedStateProps,
+  "year" | "date" | "mappedEarnedIncomeWithholdingDepartment"
+>;
+const getDefaultEmployeeChecked = ({
+  year,
+  date,
+  mappedEarnedIncomeWithholdingDepartment,
+}: GetDefaultEmployeeCheckedProps) => {
+  const resign = date[year]?.resign;
+  return mappedEarnedIncomeWithholdingDepartment!.map((total, idx) => {
+    if (!date[year]?.start) return false;
+
+    const startDate = Date.parse(date[year].start);
+    const monthsLastDay = Date.parse(
+      `${year}.${(idx + 1).toString().padStart(2, "0")}.${getDays(+year, idx)}`
+    );
+    if (startDate > monthsLastDay) return false;
+    if ((!resign || Date.parse(resign) > monthsLastDay) && total === 0)
+      return true;
+    if (resign && isRetired(resign, year, idx + 1) && total) return true;
+    return false;
+  });
+};
 
 export type WorkingDaysStateProps = {
   date: Employee["date"];
